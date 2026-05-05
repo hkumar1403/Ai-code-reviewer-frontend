c94b77f16d938f86c1f2e0dcf6567bec67f7da33
hkumar1403 <kumarhemant1403@gmail.com>
2026-05-05 12:20:11 +0530

    added backend url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://ai-code-reviewer-6av0.onrender.com",
-
-  baseURL: "http://localhost:3000",
+  baseURL: "https://ai-code-reviewer-6av0.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
